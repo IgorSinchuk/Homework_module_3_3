0f4ed8bdf297975a7928c18795e2141da5dc4e73
Igor <OldPal@users.noreply.github.com>
2016-03-13 14:28:35 +0200

    Added files via upload

---
+++ Music_instrument.java
@@ -0,0 +1,36 @@
+import java.util.List;
+
+public class Music_instrument {
+    public List<Guitar> Guitars;
+    public List<Piano> Pianos;
+    public List<Trumpet> Trumpets;
+
+    public List<Guitar> getGuitars() {
+        return Guitars;
+    }
+
+    public void setGuitars(List<Guitar> Guitars) {
+        this.Guitars = Guitars;
+    }
+
+    public List<Piano> getPianos() {
+        return Pianos;
+    }
+
+    public void setPianos(List<Piano> pianos) {
+        this.Pianos=Pianos;
+    }
+
+    public List<Trumpet> getTrumpets() {
+        return Trumpets;
+    }
+
+    public void setTrumpets(List<Trumpet> trumpets) {
+        this.Trumpets =Trumpets;
+    }
+}
+
+
+
+
+
